6ec871e2c5a54f4f34da1415ff3f69e65d70e14a
Scott <scott@scotttrinh.com>
2019-09-23 09:55:47 -0400

    Get options from options storage

--- src/api.ts
+++ src/api.ts
@@ -1,9 +1,8 @@
+import * as O from "fp-ts/lib/Option";
+import * as R from "fp-ts/lib/Record";
 import { GraphQLClient } from "graphql-request";
-import { JsonObject } from "type-fest";
-interface GraphQLResponse {
-  data: JsonObject;
-  errors: { message: string }[];
-}
+import { getAll } from "./options-storage";
+import { pipe } from "fp-ts/lib/pipeable";
 
 class RefinedLinearError extends Error {
   constructor(...messages: string[]) {
@@ -14,18 +13,27 @@ class RefinedLinearError extends Error {
 const LINEAR_URL = "https://api.linear.app/graphql";
 
 export async function query(q: string): Promise<any> {
-  const token = await storage.get("token");
-  const teamId = await storage.get("teamId");
+  const options = await getAll();
 
-  const client = new GraphQLClient(LINEAR_URL, {
-    headers: {
-      Authorization: token
-    }
-  });
-  try {
-    const response = await client.request(`{ team(id: "${teamId}") { ${q} } }`);
-    return response;
-  } catch (e) {
-    throw new RefinedLinearError(e.message);
-  }
+  pipe(
+    options,
+    R.sequence(O.option),
+    O.fold(
+      () => { throw new RefinedLinearError('token and team ID not set!') },
+      async ({token, teamId}) => {
+        const client = new GraphQLClient(LINEAR_URL, {
+          headers: {
+            Authorization: token
+          }
+        });
+        try {
+          const response = await client.request(`{ team(id: "${teamId}") { ${q} } }`);
+          return response;
+        } catch (e) {
+          throw new RefinedLinearError(e.message);
+        }
+      }
+    )
+  );
+
 }
