e8c2dc3d6323c4583ce9593528ece815b1497ce1
Scott <scott@scotttrinh.com>
2019-09-24 07:23:51 -0400

    More cleanup

--- src/features/show-board-estimation.tsx
+++ src/features/show-board-estimation.tsx
@@ -4,6 +4,7 @@ import { constant, constFalse } from "fp-ts/lib/function";
 import * as E from "fp-ts/lib/Either";
 import * as O from "fp-ts/lib/Option";
 import * as A from "fp-ts/lib/Array";
+import * as R from "fp-ts/lib/Record";
 import { pipe } from "fp-ts/lib/pipeable";
 
 import * as when from "../when";
@@ -12,6 +13,7 @@ import * as selectors from "../selectors";
 import { query } from "../api";
 import "./show-board-estimation.css";
 import { isBoard } from "../page-detect";
+import { getLastSemigroup } from "fp-ts/lib/Semigroup";
 
 const nullableNumberToEstimate = new t.Type(
   "nullableNumberToEstimate",
@@ -37,26 +39,26 @@ interface EffortProps {
   estimate: number;
 }
 
+const groupIssueEstimatesBy = R.fromFoldableMap(getLastSemigroup<IssueEstimate>(), A.array);
+
 const Effort = (props: EffortProps) =>
   ((
-    <div className="rl__effort__container">
-      <div className="rl__effort__outline">
-        <svg
-          className="rl__effort__icon"
-          width="14"
-          height="14"
-          viewBox="0 0 14 14"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <circle cx="7" cy="7" r="6.5" stroke="#BEC2C8" fill="none" />
-          <path
-            d="M10 9.2L7.3 6.7v-3a.6.6 0 10-1.2 0V7c0 .2 0 .3.2.4l3 2.7a.6.6 0 00.7 0 .6.6 0 000-.9z"
-            fill="#BEC2C8"
-          />
-        </svg>
-        <span className="rl__effort__number">{props.estimate}</span>
-      </div>
+    <div className='rl__effort__container'>
+      <svg
+        className="rl__effort__icon"
+        width="14"
+        height="14"
+        viewBox="0 0 14 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle cx="7" cy="7" r="6.5" stroke="#BEC2C8" fill="none" />
+        <path
+          d="M10 9.2L7.3 6.7v-3a.6.6 0 10-1.2 0V7c0 .2 0 .3.2.4l3 2.7a.6.6 0 00.7 0 .6.6 0 000-.9z"
+          fill="#BEC2C8"
+        />
+      </svg>
+      <span className="rl__effort__number">{props.estimate}</span>
     </div>
   ) as unknown) as HTMLDivElement;
 
@@ -77,30 +79,33 @@ issues {
   }
 };
 
-const attachEffort = (maybeEstimates: E.Either<t.Errors, IssueEstimate[]>) => (
+function getIssueNumberFromCard(cardEl: Element): O.Option<Number> {
+  return pipe(
+    O.fromNullable(cardEl.getAttribute("href")),
+    O.chain(href => O.fromNullable(href.match(/\d+$/))),
+    O.chain(A.head),
+    O.map(Number)
+  );
+}
+
+const attachEffort = (estimatesByNumber: Record<string, IssueEstimate>) => (
   issueEl: Element
 ) => {
   pipe(
-    maybeEstimates,
-    E.map(estimates => {
+    O.fromNullable(
+      issueEl.querySelector(
+        "div:nth-child(2) > div:nth-child(2) > div:nth-child(1)"
+      )
+    ),
+    O.map(overflowEl => {
       pipe(
-        O.fromNullable(issueEl.childNodes[1]),
-        O.chain(children => O.fromNullable(children.childNodes[1])),
-        O.chain(children => O.fromNullable(children.childNodes[0])),
-        O.map(el => {
-          pipe(
-            O.fromNullable(issueEl.getAttribute("href")),
-            O.chain(href => O.fromNullable(href.match(/\d+$/))),
-            O.chain(A.head),
-            O.map(Number),
-            O.chain(issueNumber =>
-              O.fromNullable(estimates.find(e => e.number === issueNumber))
-            ),
-            O.map(({ estimate }) => {
-              const effort = Effort({ estimate });
-              el.after(effort);
-            })
-          );
+        getIssueNumberFromCard(issueEl),
+        O.chain(issueNumber =>
+          R.lookup(String(issueNumber), estimatesByNumber)
+        ),
+        O.map(({ estimate }) => {
+          const effort = Effort({ estimate });
+          overflowEl.replaceWith(effort);
         })
       );
     })
@@ -109,10 +114,16 @@ const attachEffort = (maybeEstimates: E.Either<t.Errors, IssueEstimate[]>) => (
 
 const doShowBoardEstimation = async () => {
   const issueEls = Array.from(document.querySelectorAll(selectors.CARD));
-  const estimates = await getEstimates();
+  const maybeEstimates = await getEstimates();
   pipe(
-    issueEls,
-    A.map(attachEffort(estimates))
+    maybeEstimates,
+    E.map(estimates => {
+      const estimatesByNumber = groupIssueEstimatesBy(estimates, e => [String(e.number), e]);
+      pipe(
+        issueEls,
+        A.map(attachEffort(estimatesByNumber))
+      );
+    })
   );
 };
 const undo = constant(Promise.resolve());
