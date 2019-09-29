a0eec7e05fda6374c53454291b1f137c1d18fafe
Scott <scott@scotttrinh.com>
2019-09-29 17:17:01 -0400

    Use functional construct for form syncing

--- src/options.ts
+++ src/options.ts
@@ -1,12 +1,17 @@
+import * as O from "fp-ts/lib/Option";
+import { pipe } from "fp-ts/lib/pipeable";
+
+import { select } from "./utils";
 import { storage } from "./options-storage";
 
-async function init(): Promise<void> {
-  const form = document.querySelector('form');
-  if (!form) {
-    throw new Error('Could not find options form!');
-  }
-
-  storage.syncForm(form);
+function init(): void {
+  pipe(
+    select('form'),
+    O.fold(
+      () => { throw new Error("Could not find options form!") },
+      storage.syncForm
+    )
+  );
 }
 
 init();
