a09a20790122dde22dd9f2ba47ecaeaf3af364be
Scott <scott@scotttrinh.com>
2019-09-29 16:52:14 -0400

    More direct testing of IO decoders

--- src/io/number-from-nullable.spec.ts
+++ src/io/number-from-nullable.spec.ts
@@ -1,24 +1,17 @@
 import test from "ava";
 import * as E from "fp-ts/lib/Either";
-import { pipe } from "fp-ts/lib/pipeable";
 
 import { numberFromNullable } from "./number-from-nullable";
 
 const toZero = numberFromNullable(0);
 
 test("numberFromNullable", t => {
-  t.true(
-    pipe(
-      null,
-      toZero.decode,
-      E.exists(val => val === 0)
-    )
+  t.deepEqual(
+    toZero.decode(null),
+    E.right(0)
   );
-  t.true(
-    pipe(
-      1,
-      toZero.decode,
-      E.exists(val => val === 1)
-    )
+  t.deepEqual(
+    toZero.decode(1),
+    E.right(1)
   );
 });
--- src/io/option-from-empty-string.spec.ts
+++ src/io/option-from-empty-string.spec.ts
@@ -1,23 +1,16 @@
 import test from "ava";
+import * as O from "fp-ts/lib/Option";
 import * as E from "fp-ts/lib/Either";
-import { pipe } from "fp-ts/lib/pipeable";
 
 import { optionFromEmptyString } from "./option-from-empty-string";
-import { isNone, isSome } from "fp-ts/lib/Option";
 
 test("optionFromEmptyString", t => {
-  t.true(
-    pipe(
-      "",
-      optionFromEmptyString.decode,
-      E.exists(isNone)
-    )
+  t.deepEqual(
+    optionFromEmptyString.decode(""),
+    E.right(O.none)
   );
-  t.true(
-    pipe(
-      "Not empty",
-      optionFromEmptyString.decode,
-      E.exists(val => isSome(val) && val.value === "Not empty")
-    )
+  t.deepEqual(
+    optionFromEmptyString.decode("Not empty"),
+    E.right(O.some("Not empty"))
   );
 });
