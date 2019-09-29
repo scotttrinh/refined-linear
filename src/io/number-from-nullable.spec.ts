06a37f8b429b0491610b5c48e5ace1c905adfa16
Scott <scott@scotttrinh.com>
2019-09-29 17:04:43 -0400

    More coverage for IO runtime type checking

--- src/io/number-from-nullable.spec.ts
+++ src/io/number-from-nullable.spec.ts
@@ -5,13 +5,29 @@ import { numberFromNullable } from "./number-from-nullable";
 
 const toZero = numberFromNullable(0);
 
-test("numberFromNullable", t => {
+test("numberFromNullable#decode", t => {
+  t.deepEqual(toZero.decode(null), E.right(0));
+  t.deepEqual(toZero.decode(1), E.right(1));
   t.deepEqual(
-    toZero.decode(null),
-    E.right(0)
+    toZero.decode("Some string"),
+    E.left([
+      {
+        value: "Some string",
+        message: undefined,
+        context: [{ actual: "Some string", key: "", type: toZero }]
+      }
+    ])
   );
-  t.deepEqual(
-    toZero.decode(1),
-    E.right(1)
-  );
+});
+
+test("numberFromNullable#encode", t => {
+  t.deepEqual(toZero.encode(0), null);
+  t.deepEqual(toZero.encode(1), 1);
+});
+
+test("numberFromNullable#is", t => {
+  t.true(toZero.is(0));
+  t.true(toZero.is(12));
+  t.false(toZero.is("Some string"));
+  t.false(toZero.is({}));
 });
--- src/io/option-from-empty-string.spec.ts
+++ src/io/option-from-empty-string.spec.ts
@@ -4,13 +4,22 @@ import * as E from "fp-ts/lib/Either";
 
 import { optionFromEmptyString } from "./option-from-empty-string";
 
-test("optionFromEmptyString", t => {
-  t.deepEqual(
-    optionFromEmptyString.decode(""),
-    E.right(O.none)
-  );
+test("optionFromEmptyString#decode", t => {
+  t.deepEqual(optionFromEmptyString.decode(""), E.right(O.none));
   t.deepEqual(
     optionFromEmptyString.decode("Not empty"),
     E.right(O.some("Not empty"))
   );
+});
+
+test("optionFromEmptyString#encode", t => {
+  t.deepEqual(optionFromEmptyString.encode(O.none), "");
+  t.deepEqual(optionFromEmptyString.encode(O.some("Not empty")), "Not empty");
+});
+
+test("optionFromEmptyString#is", t => {
+  t.true(optionFromEmptyString.is(O.none));
+  t.true(optionFromEmptyString.is(O.some("Not empty")));
+  t.false(optionFromEmptyString.is(O.some(12)));
+  t.false(optionFromEmptyString.is({}));
 });
