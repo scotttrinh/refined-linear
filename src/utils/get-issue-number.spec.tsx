718c4ede59fd10c20a625962dcf7b018fb47e2b7
Scott <scott@scotttrinh.com>
2019-09-29 20:02:39 -0400

    More strict matching for getting issue number from card element

--- src/utils/get-issue-number.spec.tsx
+++ src/utils/get-issue-number.spec.tsx
@@ -1,11 +1,10 @@
 import React from "dom-chef";
 import test from "ava";
 import * as O from "fp-ts/lib/Option";
-import { pipe } from "fp-ts/lib/pipeable";
 
 import { fromCard } from "./get-issue-number";
 
-import '../test-helpers/setup';
+import "../test-helpers/setup";
 
 const issueAnchorElement = ((
   <a href="https://linear.app/issue/FOO-12" />
@@ -15,18 +14,6 @@ const otherAnchorElement = ((
 ) as unknown) as Element;
 
 test("fromCard", t => {
-  t.true(
-    pipe(
-      issueAnchorElement,
-      fromCard,
-      O.exists(val => val === 12)
-    )
-  );
-  t.false(
-    pipe(
-      otherAnchorElement,
-      fromCard,
-      O.isNone
-    )
-  );
+  t.deepEqual(fromCard(issueAnchorElement), O.some(12));
+  t.deepEqual(fromCard(otherAnchorElement), O.none);
 });
--- src/utils/get-issue-number.ts
+++ src/utils/get-issue-number.ts
@@ -5,8 +5,8 @@ import { pipe } from "fp-ts/lib/pipeable";
 export function fromCard(cardEl: Element): O.Option<number> {
   return pipe(
     O.fromNullable(cardEl.getAttribute("href")),
-    O.chain(href => O.fromNullable(href.match(/\d+$/))),
-    O.chain(A.head),
+    O.chain(href => O.fromNullable(href.match(/issue\/.+-(\d+)$/))),
+    O.chain(matches => A.lookup(1, matches)),
     O.map(Number)
   );
 }
