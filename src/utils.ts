500cb2f682b29de4df5e2fd2d06dff41e3ed489a
Scott <scott@scotttrinh.com>
2019-10-16 09:15:02 -0400

    Just wrap `select-dom` for the Option select utils

--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,42 @@
+import selectNullable from "select-dom";
 import * as O from "fp-ts/lib/Option";
 
-export function select<K extends keyof HTMLElementTagNameMap>(
-  selector: K,
-  baseElement: Element | Document = document
-): O.Option<HTMLElementTagNameMap[K]> {
-  return O.fromNullable(baseElement.querySelector(selector));
+export function select<T extends keyof HTMLElementTagNameMap>(
+  selectors: T,
+  baseElement?: ParentNode
+): O.Option<HTMLElementTagNameMap[T]>;
+export function select<T extends keyof SVGElementTagNameMap>(
+  selectors: T,
+  baseElement?: ParentNode
+): O.Option<SVGElementTagNameMap[T]>;
+export function select<T extends HTMLElement = HTMLElement>(
+  selectors: string,
+  baseElement?: ParentNode
+): O.Option<T>;
+export function select(
+  selectors: string,
+  baseElement?: ParentNode
+): O.Option<Element> {
+  return O.fromNullable(selectNullable(selectors, baseElement));
 }
 
-export function selectAll<K extends keyof HTMLElementTagNameMap>(
-  selector: K,
-  baseElement: Element | Document = document
-): HTMLElementTagNameMap[K][] {
-  return Array.from(baseElement.querySelectorAll(selector));
+type BaseElements = ParentNode | ArrayLike<ParentNode>;
+
+export function selectAll<T extends keyof HTMLElementTagNameMap>(
+  selectors: T,
+  baseElements?: BaseElements
+): HTMLElementTagNameMap[T][];
+export function selectAll<T extends keyof SVGElementTagNameMap>(
+  selectors: T,
+  baseElements?: BaseElements
+): SVGElementTagNameMap[T][];
+export function selectAll<T extends HTMLElement = HTMLElement>(
+  selectors: string,
+  baseElements?: BaseElements
+): T[];
+export function selectAll(
+  selectors: string,
+  baseElements?: BaseElements
+): Element[] {
+  return selectNullable.all(selectors, baseElements);
 }
